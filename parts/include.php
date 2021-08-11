88919ef94d67336543c0a9e07fb423dc6ea546df
Riccardo <riccardo.giovarelli@gmail.com>
2021-08-11 14:14:26 +0200

    feat: turn to Laravel

--- parts/include.php
+++
@@ -1,3 +0,0 @@
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
-<link rel="stylesheet" href="/css/main.min.css">
-<link rel='icon' type='image/x-icon' href='/favicon.ico' />
--- parts/meta.php
+++
@@ -1,9 +0,0 @@
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="title" content="RG-Space">
-<meta name="description" content="Riccardo Giovarelli home page">
-<meta name="keywords" content="riccardo,giovarelli,development,blog,php,javascript,react">
-<meta name="robots" content="index, follow">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta name="language" content="English">
-<meta name="author" content="Riccardo Giovarelli">
