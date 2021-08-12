f8192b017335541e30c3511e6e9b719ba0f98f3a
Riccardo <riccardo.giovarelli@gmail.com>
2021-08-12 17:50:57 +0200

    fix: graphics fixes

--- app/resources/views/contact/contact.blade.php
+++ app/resources/views/contact/contact.blade.php
@@ -25,24 +25,24 @@
                             @csrf
                             <div class="form-group">
                                 <label for="name"
-                                    class="form-label">{{ __('contactme.contactme_input_name') }}</label>
+                                    class="form-label fs-5">{{ __('contactme.contactme_input_name') }}</label>
                                 <input type="text" class="form-control contact-form-input" required id="name"
                                     name="name">
                             </div>
                             <div class="form-group mt-4">
                                 <label for="email"
-                                    class="form-label">{{ __('contactme.contactme_input_email') }}</label>
+                                    class="form-label fs-5">{{ __('contactme.contactme_input_email') }}</label>
                                 <input type="email" class="form-control contact-form-input" required id="email"
                                     name="email" placeholder="name@example.com">
                             </div>
                             <div class="form-group mt-4">
                                 <label for="subject"
-                                    class="form-label">{{ __('contactme.contactme_input_subject') }}</label>
+                                    class="form-label fs-5">{{ __('contactme.contactme_input_subject') }}</label>
                                 <input type="text" class="form-control contact-form-input" id="subject" name="subject">
                             </div>
                             <div class="form-group mt-4">
                                 <label for="message"
-                                    class="form-label">{{ __('contactme.contactme_input_message') }}</label>
+                                    class="form-label fs-5">{{ __('contactme.contactme_input_message') }}</label>
                                 <textarea class="form-control contact-form-input" required id="message" name="message"
                                     rows="4"></textarea>
                             </div>
