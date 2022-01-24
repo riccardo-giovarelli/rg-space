2033bfbe6751d643ed1fab757cd6a85ad889bd4f
Riccardo <riccardo.giovarelli@gmail.com>
2022-01-25 00:38:34 +0100

    feat: add google analytics

--- app/resources/views/layouts/head.blade.php
+++ app/resources/views/layouts/head.blade.php
@@ -10,4 +10,16 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <script src="https://www.google.com/recaptcha/api.js"></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-6K4WF44M3T"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-6K4WF44M3T');
+    </script>
 </head>
